310ae9800383a7c12d258b575f02d4742b28afb6
Ben Asaf <ToBenAsaf@Gmail.com>
2016-11-20 08:55:03 +0200

    Sorry: now the most recent Divide.asm

--- 04/divide/Divide.asm
+++ 04/divide/Divide.asm
@@ -1,10 +1,11 @@
 // http://stackoverflow.com/questions/5386377/division-without-using
+
 // Calculate (R13-R14) and assign result to D
 @R14
 D=M
 @R13
 D=M-D
-///////////////////////
+//////////////////////////////////////////////
 // Now checking special conditions:
 @ZERO  // If R13-R14<0 THEN R13/R14<1, so go to (ZERO) where R15 will be set to 0 and "terminate" program.
 D;JLT
@@ -19,6 +20,7 @@ M=1
 @FIRST  // Go to (FIRST)
 0;JMP
 
+// (FIRST) main "function" to see how many times the denominator fits inside the dividend
 (FIRST)
 	@Current  // R14 fits inside R13 at least once, so shift left Current (Multiply by 2)
 	M=M<<
@@ -31,12 +33,13 @@ M=1
 	@R13
 	D=M-D
 	///////////////////////
-
-	@MID  // If R13-R14<0 then R13/R14<1, so go to (MID)
+	// Now checking what to do:
+	@MID  // If R13-R14<0 then R13/R14<1, go to (MID) to prepare for (SECOND) main "function"
 	D;JLT
-	@FIRST  // Else go to division (FIRST)
+	@FIRST  // Else: continue looping (FIRST)
 	0;JMP
 
+// (MID) is used to prepare for the (SECOND) process of the division
 (MID)
 	@R14
 	M=M>>
@@ -45,6 +48,7 @@ M=1
 	@SECOND
 	0;JMP
 
+// (HELPER1) "function" to build the final answer: "R15"
 (HELPER1)
 	// Do R13<--(R13-R14)
 	@R14
@@ -61,6 +65,7 @@ M=1
 	@HELPER2
 	0;JMP
 
+// (HELPER2) "function" to divide both 'Current' and 'R14' by 2, (Right Shift)
 (HELPER2)
 	@Current
 	M=M>>
@@ -70,7 +75,7 @@ M=1
 	@SECOND
 	0;JMP
 
-
+// (SECOND) main "function" to build the final answer
 (SECOND)
 	// If Current==0 Go to (END)
 	@Current  
@@ -89,17 +94,18 @@ M=1
 	@HELPER2  // Else go to HELPER2
 	0;JMP
 	
-
+// End-point: If either R13 or R14 equals 'zero'
 (ZERO)
 	@R15
 	M=0
 	@END
 	0;JMP
-
+// End-point: If R13==R14 then R13/R14==1
 (ONE)
 	@R15
 	M=1
 	@END
 	0;JMP
 
+// "End" of the program
 (END)
