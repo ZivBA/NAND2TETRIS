d0d6c5eaaf3707b2cee7dffacb91805eb8ed0860
rooty <zz>
2016-11-26 16:09:10 +0200

    fixed A inst unit

--- 06/#Solution/TranslateAinstructions.py
+++ 06/#Solution/TranslateAinstructions.py
@@ -2,17 +2,21 @@ import re
 
 
 re1 = '(@)'  # Any Single Character 1
-re2 = '(\d+)'  # Integer Number 1
+re2 = '(.*)'  # Integer Number 1
 
 rg = re.compile(re1 + re2, re.IGNORECASE | re.DOTALL)
 
 
-def TranslateAIns(asmList):
+def TranslateAIns(asmList, addressDict):
     newList = []
     for line in asmList:
         match = rg.search(line)
-        if match:
-            newList.append(format(int(match.group(2)),'016b'))
+        if match.group(1):
+            address = addressDict[match.group(2)]
+            if (address):
+                newList.append(format(int(address), '016b'))
+            else:
+                newList.append(format(int(match.group(2)), '016b'))
         else:
             newList.append(line)
     return newList
