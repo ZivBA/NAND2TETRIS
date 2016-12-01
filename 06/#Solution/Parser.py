22239342c1c1ad0488e1ea58afddd846a9b83d87
Ben Asaf <ToBenAsaf@Gmail.com>
2016-12-01 14:20:04 +0200

    Documented Main.py, Parser.py. Finished and working

--- 06/#Solution/Parser.py
+++ 06/#Solution/Parser.py
@@ -1,25 +1,23 @@
-import os
 import re
 
-#COMMENT_LINE_PATTERN = "^\s*[\/\/]+.*$"  # RE to capture comment line
-#INLINE_COMMENT_PATTERN = "^(?:\s*)([^\s]*?)(?:\s*)(?:\/\/.*)?$"  # RE to catpure inline comment
-INLINE_COMMENT_PATTERN = "^(.*?)(\/\/.+)?$"  # RE to catpure inline comment
-
-
-#RE_COMMENT_LINE = re.compile(COMMENT_LINE_PATTERN)
-RE_INLINE_COMMENT = re.compile(INLINE_COMMENT_PATTERN)
-
-EMTPY_LINE = ""  # New line character
-
+CONTENT_EXTRACT_PATTERN = "^(.*?)(\/\/.+)?$"  # RE to capture the content and ignore the reset
+RE_CONTENT_EXTRACT = re.compile(CONTENT_EXTRACT_PATTERN)
+EMTPY_LINE = ""  # Empty line
 
 
 def PreprocessFile(LINES):
+    """
+    This function extracts the actual relevant content. Meaning it completely ignores Comments, In-line comments
+    and empty lines.
+    :param LINES: The content of the '.asm' file after being processed by '.splitlines()' function
+    :return: The content of the file in plain Hack syntax
+    """
     FILE_CONTENT = []
 
     for line in LINES:
-        newLine = RE_INLINE_COMMENT.match(line).group(1).replace(" ", "")
+        # Extracting the content, replacing all spaces with "" to promote cleanliness of the Hack syntax
+        newLine = RE_CONTENT_EXTRACT.match(line).group(1).replace(" ", "")
         if (newLine == EMTPY_LINE):  # If an empty line --> Skip it.
             continue
-        FILE_CONTENT.append(newLine)
-
+        FILE_CONTENT.append(newLine)  # Else, add the content that was extracted.
     return FILE_CONTENT
