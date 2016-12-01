68d76fb4df22ec8bf94fdc0b2e90834c1e131e77
rooty <zz>
2016-12-01 13:03:20 +0200

    changed main

--- 06/#Solution/Main.py
+++ 06/#Solution/Main.py
@@ -1,43 +1,51 @@
-import Parser, BuildSymbolTable, TranslateAinstructions, TranslateCInstructions
-import sys, os
+import Parser
+import BuildSymbolTable
+import TranslateAinstructions
+import TranslateCInstructions
+import sys
+import os
 
 FILE_TYPE = ".hack"
 
+
 def getFileName(pathToFile):
-	path, fileName = os.path.split(pathToFile)
-	fileName = fileName.split(".")
-	return path, fileName[0]
+    path, fileName = os.path.split(os.path.abspath(pathToFile))
+    fileName = fileName.split(".")
+    return path, fileName[0]
+
 
 def PrintContent(CONTENT):
-	for x in CONTENT:
-		print(x)
+    for x in CONTENT:
+        print(x)
+
 
 def writeToFile(CONTENT, PATH, FILE_NAME):
-	"""
+    """
 
-	:param CONTENT:
-	:param PATH:
-	:param FILE_NAME:
-	:return:
-	"""
-	with open(PATH + os.sep + FILE_NAME + FILE_TYPE, mode="w+") as f:
-		for k in range(len(CONTENT)):
-			f.write(CONTENT[k])
-			if (k < len(CONTENT) - 1):  # Avoiding empty line at EOF
-				f.write("\n")
+    :param CONTENT:
+    :param PATH:
+    :param FILE_NAME:
+    :return:
+    """
+    with open(PATH + os.sep + FILE_NAME + FILE_TYPE, mode="w+") as f:
+        for k in range(len(CONTENT)):
+            f.write(CONTENT[k])
+            if (k < len(CONTENT) - 1):  # Avoiding empty line at EOF
+                f.write("\n")
+
 
 def main(argv):
-	with open(argv[1], mode="r") as f:
-		CONTENT = Parser.PreprocessFile(f)
+    with open(argv[1], mode="r") as f:
+        CONTENT = Parser.PreprocessFile(f)
 
-	addrDict, newList = BuildSymbolTable.BuildSymbolTable(CONTENT)
+    addrDict, newList = BuildSymbolTable.BuildSymbolTable(CONTENT)
 
-	newList = TranslateAinstructions.TranslateAIns(newList, addrDict)
+    newList = TranslateAinstructions.TranslateAIns(newList, addrDict)
 
-	finalOutput = TranslateCInstructions.TranslateCIns(newList)
+    finalOutput = TranslateCInstructions.TranslateCIns(newList)
 
-	outputPath, outputName = getFileName(argv[1])
-	writeToFile(finalOutput, outputPath, outputName)
+    outputPath, outputName = getFileName(argv[1])
+    writeToFile(finalOutput, outputPath, outputName)
 
 # Run main function:
 main(sys.argv)
